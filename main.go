85a9883583a36fa4f640a16d3988e5f762fa286e
mehkij <disismehki@gmail.com>
2024-11-17 07:01:46 -0500

    Add CLI functionality

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mehkij/blog-aggregator/internal/config"
+)
+
+type state struct {
+	config *config.Config
+}
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	programState := &state{
+		config: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cli <command> [args...]")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
