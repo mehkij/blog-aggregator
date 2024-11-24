ae57ff77f8cf2685367ea82d4b54c95ffdfe5e14
mehkij <disismehki@gmail.com>
2024-11-23 22:24:30 -0500

    Create register command handler

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mehkij/blog-aggregator/internal/database"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -15,4 +23,40 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Println("User successfully logged in!")
 
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	username, err := s.db.GetUser(context.Background())
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	if username == cmd.Args[0] {
+		fmt.Println("User already exists!")
+		os.Exit(1)
+		return nil
+	}
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: cmd.Args[0]})
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	err = s.config.SetUser(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't set the current user: %w", err)
+	}
+
+	fmt.Println("User successfully registered!")
+	printUser(user)
+
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
