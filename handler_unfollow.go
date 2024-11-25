02b212ca7759f41e176168cf7dcf3fff8ca32bb4
mehkij <disismehki@gmail.com>
2024-11-24 23:42:54 -0500

    Create new command: unfollow

---
+++ handler_unfollow.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mehkij/blog-aggregator/internal/database"
+)
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <url>", cmd.Name)
+	}
+
+	err := s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		Url:    cmd.Args[0],
+	})
+	if err != nil {
+		return fmt.Errorf("could not unfollow: %w", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 	cmds.register("feeds", handlerListFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerFollowing))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 
 	// 1st arg is program name, which we ignore.
 	if len(os.Args) < 2 {
